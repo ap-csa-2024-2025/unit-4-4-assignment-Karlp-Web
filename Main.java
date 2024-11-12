77b2ec50f889a82b547a51893cf0fac2349a0cd6
Karlp-Web <karlenportillo1@gmail.com>
2024-11-12 19:02:22 +0000

    yomomma

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ public class Main
   public static void main(String[] args)
   {
     String sentence = "peter piper picked a pack of pickle peppers";
+    int count = 0;
     for (int i = 0; i < sentence.length() - 1; i++)
     {
       String pair = sentence.substring(i, i+2);
