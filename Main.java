13bfdc094673c05509e21998ea5b2cca2bfe0ae0
Karlp-Web <karlenportillo1@gmail.com>
2024-11-12 19:03:32 +0000

    yomomma

--- Main.java
+++ Main.java
@@ -7,14 +7,14 @@ public class Main
     String sentence = "peter piper picked a pack of pickle peppers";
     int count = 0;
     for (int i = 0; i < sentence.length() - 1; i++)
-    {
+   {
       String pair = sentence.substring(i, i+2);
-      if (pair.equals("pa") || pair.equals("pe") || pair.equals("po") || pair.equals("pi"));
+      if (pair.equals("pa") || pair.equals("pe") || pair.equals("po") || pair.equals("pi"))
  2     {
         count++;
-      }
+       }
       System.out.println("p followed by a vowel appears" + count + "times");
-    }
+   }
   }
 
   public static boolean isPrime(int N)
